0e8fd39d3860d4853887f2a8938f88d696d41348
Sularada <111848753+Sularada@users.noreply.github.com>
2025-05-14 10:25:39 +0300

    http request error handling

--- e-commerce--app/src/app/services/product.service.ts
+++ e-commerce--app/src/app/services/product.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private http:HttpClient) { }
-  public getProducts(): Observable<Product[]>{
-    return this.http.get<Product[]>("http://localhost:3000/products")
+  constructor(private http: HttpClient) { }
+  public getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>("http://localhost:3000/products").pipe(tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = ''
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Bir hata oluştu ' + err.error.message
+    } else {
+      errorMessage = 'Sistemsel bir hata oluştu'
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
